c76e904544547eec8e1a236d9b47885d4b74b551
jago4232 <jagoez@hotmail.com>
2024-11-28 09:34:57 -0500

    Upload microservices

---
+++ MicroserviceItem/src/main/java/com/bancolombia/app/services/ServiceImpl.java
@@ -0,0 +1,25 @@
+package com.bancolombia.app.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.bancolombia.app.datasource.ItemRepository;
+import com.bancolombia.app.entities.Item;
+
+@Service
+public class ServiceImpl implements IService {
+	@Autowired
+	private ItemRepository repository;
+
+	@Override
+	public boolean insert(Item item) {
+		return repository.insert(item);
+	}
+
+	@Override
+	public List<Item> getAll() {
+		return repository.getAll();
+	}
+	
+
+}
